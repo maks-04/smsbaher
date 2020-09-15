09181da8f3695eaa417aec18e0a83cedfb68c61f
ant@skin <68486524+Antaskin@users.noreply.github.com>
2020-09-15 17:52:03 +0300

    Update smsbaher.py

--- smsbaher.py
+++ smsbaher.py
@@ -25,15 +25,10 @@ banner = """ \x1b[31m
 print(banner)
 _phone = input(' \x1b[44m Привет! \x1b[5m Пиши номер для атаки Создатель Вк \x1b[41m @antiaskin \x1b[0m (\x1b[31m79xxxxxxxxx\x1b[0m)-->> ')
 
-if _phone[1] == '79271703628':
-	print ('hui')
+if (phone[0:2] == "7" and len(phone[1:]) == 11) or (phone[0:2] == "79271703628" and len(phone[1:]) == 13):
+
 else:
-	if _phone[0] == '+':
-		_phone = _phone[1:]
-if _phone[0] == '8':
-	_phone = '7'+_phone[1:]
-if _phone[0] == '9':
-	_phone = '7'+_phone
+    print("Номер телефона набран не правильно, повторите попытку")
 
 _name = ''
 for x in range(12):
